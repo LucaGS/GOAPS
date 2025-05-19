26bd4201731ed161cce8c57d897ba7968efe8329
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-05-19 19:53:56 +0200

    Product in/output

--- src/main/java/GastroOrderAndPaySystem/GOAPS/controller/MenuController.java
+++ src/main/java/GastroOrderAndPaySystem/GOAPS/controller/MenuController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import GastroOrderAndPaySystem.GOAPS.model.Product;
 import GastroOrderAndPaySystem.GOAPS.repository.ProductRepository;
 
 @Controller
@@ -17,5 +20,15 @@ public class MenuController {
     public String showMenu(Model model) {
         model.addAttribute("products", productRepository.findAll());
         return "menu"; // Loads templates/menu.html
+    }
+      @GetMapping("/new")
+    public String showAddForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "addProduct"; // loads addProduct.html
+    }
+     @PostMapping("/products")
+    public String addProduct(@ModelAttribute Product product) {
+        productRepository.save(product);
+        return "redirect:/";
     }
 }
